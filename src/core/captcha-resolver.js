0a75b5a7f3b46cbcb517bcefd8fd08e2b777a00b
Leo Falco <leonardo.falco@outlook.com>
2021-08-08 11:48:19 -0300

    feat: la vai

--- src/core/captcha-resolver.js
+++ src/core/captcha-resolver.js
@@ -1,7 +1,39 @@
 'use strict'
 
-const axios = require('axios')
+const { default: axios } = require('axios')
+const ms = require('ms')
 const { delay } = require('./delay')
+
+const MAX_ATTEMPTS = 20
+
+function isError (response) {
+  return String(response.data.request).startsWith('ERROR')
+}
+
+function isResolved (response) {
+  return String(response.data.status) === '1'
+}
+
+function extractRequestId (response) {
+  return String(response.data.request)
+}
+
+function extractCapchaResponse (response) {
+  return String(response.data.request)
+}
+
+class ServiceError extends Error {
+  constructor (response) {
+    const message = String(response.data.request || 'Unknown error')
+    super(`Capcha resolver service error: ${message}`)
+  }
+}
+
+class MaxAttemptsError extends Error {
+  constructor () {
+    super('Capcha resolver service max attempts err')
+  }
+}
 
 class CaptchaResolver {
   constructor ({ apiKey, baseURL }) {
@@ -11,47 +43,60 @@ class CaptchaResolver {
     })
   }
 
-  async sendChallenge ({ pageurl, googleKey }) {
-    const response = await this.client.post('in.php', null, {
+  async startCapchaResolution ({ pageurl, googleKey }) {
+    return this.client.request({
+      method: 'POST',
+      url: 'in.php',
       params: {
         key: this.apiKey,
-        method: 'userrecaptcha',
         googlekey: googleKey,
-        json: 1,
-        pageurl: pageurl
+        pageurl: pageurl,
+        method: 'userrecaptcha',
+        json: 1
       }
     })
-
-    console.log('challenge sent', response.data)
-
-    return response.data.request.toString()
   }
 
-  async getChallengeResponse ({ requestId }) {
-    while (true) {
-      await delay(15000)
+  async verifyCapchaResponse ({ requestId }) {
+    return this.client.request({
+      method: 'POST',
+      url: 'res.php',
+      params: {
+        key: this.apiKey,
+        id: requestId,
+        action: 'get',
+        json: 1,
+        header_acao: 1
+      }
+    })
+  }
 
-      const response = await this.client.post('res.php', null, {
-        params: {
-          key: this.apiKey,
-          action: 'get',
-          id: requestId,
-          json: 1,
-          header_acao: 1
-        }
-      })
+  async resolveCapcha ({ pageurl, googleKey }) {
+    const requestId = await this.startCapchaResolution({ pageurl, googleKey })
+      .then(extractRequestId)
+    console.log('requestId', requestId)
 
+    let attempts = 0
+    while (attempts < MAX_ATTEMPTS) {
+      await delay(ms('5s'))
+
+      console.log('attempts', attempts)
+
+      const response = await this.verifyCapchaResponse({ requestId })
       console.log('response', response.data)
 
-      if (response.data.request.startsWith('ERROR')) {
-        throw new Error(`capcha resolver service returned error ${response.data.request}`)
+      if (isError(response)) {
+        throw new ServiceError(response)
       }
 
-      if (response.data.status === 1) {
-        console.log('captcha solved')
-        return response.data.request
+      if (isResolved(response)) {
+        return extractCapchaResponse(response)
       }
+
+      attempts++
     }
+
+    throw new MaxAttemptsError()
   }
 }
 
--- src/handlers/hit-point/handler.js
+++ src/handlers/hit-point/handler.js
@@ -1,74 +1,107 @@
 'use strict'
 
 const chromium = require('chrome-aws-lambda')
+const ms = require('ms')
 const { URL } = require('url')
 const { CaptchaResolver } = require('../../core/captcha-resolver')
 const { delay } = require('../../core/delay')
+const fetch = require('node-fetch')
 
 const CAPTCHA_API_KEY = '6f22570623a8b7eb2158155f11f171a0'
 const GENYO_URL = 'https://app.genyo.com.br?aba=registrarPonto'
 const CAPTCHA_RESOLVER_URL = 'http://2captcha.com'
 
-async function handler () {
-  let browser = null
+async function closeBrowser (browser) {
+  if (browser) {
+    await browser.close().catch()
+  }
+}
+
+async function getDataFromNavigatorPage () {
+  const browser = await chromium.puppeteer.launch({
+    headless: true
+  })
+  console.log('browser created')
 
   try {
-    browser = await chromium.puppeteer.launch({
-      headless: chromium.headless
-    })
-
     const page = await browser.newPage()
+    console.log('new page created')
 
-    await page.goto(GENYO_URL)
-    await delay(2000)
-
-    const captchaSrc = await page.evaluate(() => {
-      document.querySelector('#codigoEmpresa2').value = '2WRBK'
-      document.querySelector('#numeroAcesso2').value = '629268'
-      return document.querySelector('iframe').src
+    await page.goto(GENYO_URL, {
+      waitUntil: 'load',
+      timeout: ms('60s')
     })
 
-    const url = new URL(captchaSrc)
+    await delay(ms('1s'))
+    console.log('navigated to genio url')
 
-    const googleKey = url.searchParams.get('k')
-    const captchaResolver = new CaptchaResolver({
+    const { captchaSrc, cookie } = await page.evaluate(() => {
+      const iframe = document.querySelector('iframe')
+      return {
+        captchaSrc: iframe.src,
+        cookie: document.cookie
+      }
+    })
+
+    await closeBrowser(browser)
+
+    const capchaUrl = new URL(captchaSrc)
+    const googleKey = capchaUrl.searchParams.get('k')
+
+    return {
+      googleKey,
+      cookie
+    }
+  } catch (err) {
+    await closeBrowser(browser)
+    throw err
+  }
+}
+
+async function handler () {
+  const browser = null
+
+  try {
+    const { googleKey, cookie } = await getDataFromNavigatorPage()
+    console.log('googleKey', googleKey)
+    console.log('cookie', cookie)
+
+    const capchaResponseToken = await new CaptchaResolver({
       apiKey: CAPTCHA_API_KEY,
       baseURL: CAPTCHA_RESOLVER_URL
-    })
-
-    const requestId = await captchaResolver.sendChallenge({
+    }).resolveCapcha({
       googleKey: googleKey,
       pageurl: GENYO_URL
     })
 
-    const capchaResponse = await captchaResolver.getChallengeResponse({
-      requestId: requestId
-    })
+    console.log('capchaResponseToken', capchaResponseToken)
 
-    console.log('process.env.POINT_TYPE', process.env.POINT_TYPE)
+    const pointType = process.env.POINT_TYPE
 
-    await page.evaluate((capchaResponse, pointType) => {
-      document.querySelector('iframe').click()
+    const reponseHeaders = await fetch('https://app.genyo.com.br/registrarPonto', {
+      method: 'POST',
+      referrer: 'https://app.genyo.com.br/?aba=abaColaborador',
+      referrerPolicy: 'strict-origin-when-cross-origin',
+      body: `codigoEmpresa=2WRBK&numeroAcesso=629268&foto=&observacao=&g-recaptcha-response=${capchaResponseToken}&${pointType}=1`,
+      mode: 'cors',
+      headers: {
+        accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+        'accept-language': 'pt-BR,pt;q=0.9,es;q=0.8,en;q=0.7',
+        'cache-control': 'no-cache',
+        'content-type': 'application/x-www-form-urlencoded',
+        pragma: 'no-cache',
+        'sec-ch-ua': '"Chromium";v="92", " Not A;Brand";v="99", "Google Chrome";v="92"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-fetch-dest': 'document',
+        'sec-fetch-mode': 'navigate',
+        'sec-fetch-site': 'same-origin',
+        'sec-fetch-user': '?1',
+        'upgrade-insecure-requests': '1',
+        cookie: cookie
+      }
+    }).then(response => response.headers)
 
-      const captchaResponseElement = document.querySelector('#g-recaptcha-response')
-
-      captchaResponseElement.innerHTML = capchaResponse
-
-      // console.log('captchaResponseElement', captchaResponseElement)
-
-      // if (pointType === 'start') {
-      //   document.querySelector('button[data-tipo=entrada').click()
-      // }
-
-      // if (pointType === 'stop') {
-      //   document.querySelector('button[data-tipo=saida').click()
-      // }
-      // document.querySelector('button[data-acao=continuarPonto').click()
-    }, capchaResponse, process.env.POINT_TYPE)
-
-    await delay(1000000)
-
-    await browser.close()
+    console.log('reponseHeaders', reponseHeaders)
 
     return {
       code: 'ok'
